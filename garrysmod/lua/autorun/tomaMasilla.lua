e3aa2ca77c6ee5b181bd846e49f2357fbaee9c1b
Isabel Arrans Vega <67335367+iarrans@users.noreply.github.com>
2021-02-27 20:57:59 +0100

    Delete garrysmod directory

--- garrysmod/lua/autorun/tomaMasilla.lua
+++
@@ -1,8 +0,0 @@
-sound.Add( {
-    name = "sounds/chairGun/tomaSilla",
-    channel = CHAN_STATIC,
-    volume = 1.0,
-    level = 80,
-    pitch = {95, 110},
-    sound = "sounds/chairGun/tomaSilla.wav"
-} )
